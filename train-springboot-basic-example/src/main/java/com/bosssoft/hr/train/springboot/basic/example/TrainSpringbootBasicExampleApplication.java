65627012a31606b66ccd4b54696258a064526f2d
abel.zhan <276895816@qq.com>
2020-06-18 22:08:02 +0800

    init traning example

---
+++ train-springboot-basic-example/src/main/java/com/bosssoft/hr/train/springboot/basic/example/TrainSpringbootBasicExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bosssoft.hr.train.springboot.basic.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainSpringbootBasicExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainSpringbootBasicExampleApplication.class, args);
+    }
+
+}
